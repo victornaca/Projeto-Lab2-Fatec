8d352b264b855b13c79802fa0c7cc4041ab837e5
Victor Fernandes <victor.santos136@fatec.sp.gov.br>
2023-08-27 14:46:49 -0300

    teste criacao pacotes

---
+++ projeto.quarkus/src/main/java/fatec/lp/entity/GreetingResource.java
@@ -0,0 +1,15 @@
+package fatec.lp.entity;
+
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.core.MediaType;
+
+@Path("/hello")
+public class GreetingResource {
+
+    @GET
+    public String hello() {
+        return "Hello, World!";
+    }
+}
