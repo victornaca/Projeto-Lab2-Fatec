7ddd6c26579110cf55dae3a2de905195616287a3
Victor Fernandes <victor.santos136@fatec.sp.gov.br>
2023-11-11 14:36:23 -0300

    colocar DTO e arrumar service e resource do lance

---
+++ projeto.quarkus/src/main/java/br/gov/sp/fatec/dto/LanceDTO.java
@@ -0,0 +1,15 @@
+package br.gov.sp.fatec.dto;
+
+import java.util.Date;
+
+import lombok.Data;
+
+@Data
+public class LanceDTO {
+
+    private Long veiculoId;
+    private Long dispositivoId;
+    private Long clienteId;
+    private Double valor;
+
+}
